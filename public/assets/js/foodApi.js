fc3a758cee9c3d7bd5e7b2ee89b6799509f36f5c
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-03-17 13:47:41 -0500

    try again

--- public/assets/js/foodApi.js
+++ public/assets/js/foodApi.js
@@ -26,7 +26,7 @@ $(function () {
                         var result = response.results;
 
                         console.log(result)
-                        for (var i = 0; i < 3; i++) {
+                        for (var i = 0; i < 2; i++) {
                     
                             var photoRef = result[i].photos[0].photo_reference;
                             var photoURL = getPhotoURLByReference(photoRef);
@@ -34,11 +34,21 @@ $(function () {
                             var restRating = result[i].rating;
                             var restAddress = result[i].vicinity;
 
-                            var newRow = $("<div class='row restaurant'>");
-                            var newRestDiv = $("<div>").addClass("col-md-12")
+                            var newRestDiv = $("<div>").addClass("row restaurant")
+                            var restNameDiv = $("<h4>").text(restName);
+                            var restAddressDiv = $("<p>").text(restAddress);
+                            var restRatingDiv = $("<p>").text(restRating);
+                            var button = $("<button class='btn-primary'>").text("Save");
+                            var imgDiv = $(`<img src=${photoURL}>`)
 
-                            var photo_col = $(`<img src=${photoURL}>`)
-                            newRestDiv.append(restName, restRating, restAddress, photo_col)
+                            var photo_col = $("<div>").addClass("col-md-3")
+                            var info_col = $("<div>").addClass("col-md-3")
+                            var select_col = $("<div>").addClass("col-md-6")
+
+                            photo_col.append(imgDiv);
+                            info_col.append(restNameDiv, restAddressDiv, restRatingDiv);
+                            select_col.append(button);
+                            newRestDiv.append(photo_col, info_col, select_col)
                             $(".diningOptions").append(newRestDiv);
 
 
