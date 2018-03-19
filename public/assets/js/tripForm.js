f082974a8836e318f658ec0bbc069c12ca377a5f
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-03-18 20:15:53 -0500

    crispyyyy

--- public/assets/js/tripForm.js
+++ public/assets/js/tripForm.js
@@ -1,3 +1,38 @@
+var locationCity = $("#locality").val();
+var zipCode = $("#postal_code").val();
+
+var dateFormat = "mm/dd/yy",
+  from = $("#from")
+    .datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3
+    })
+    .on("change", function() {
+      to.datepicker("option", "minDate", getDate(this));
+    }),
+  to = $("#to")
+    .datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3
+    })
+    .on("change", function() {
+      from.datepicker("option", "maxDate", getDate(this));
+    });
+
+function getDate(element) {
+  var date;
+  try {
+    date = $.datepicker.parseDate(dateFormat, element.value);
+  } catch (error) {
+    date = null;
+  }
+
+  return date;
+}
+
+// Google's AutoComplete Feature
 // This example displays an address form, using the autocomplete feature
 // of the Google Places API to help users fill in the information.
 
