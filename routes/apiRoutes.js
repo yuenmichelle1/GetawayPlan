b2febba8bcac3dd7459637a6ed31fd525b3d1b20
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-03-21 15:00:49 -0500

    able to redirect signin signup home to dashboard once user sign in

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,12 +5,6 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-        if (req.user) {
-            res.redirect("/api/trip/dashboard");
-        }
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
 
     app.post("/api/login", passport.authenticate("local"), function(req, res) {
         res.json("/api/trip/dashboard");
@@ -51,7 +45,6 @@ module.exports = function (app) {
     })
 
     app.post("/api/signup", function (req, res) {
-        console.log(req.body);
         db.User.create({
             email: req.body.email,
             password: req.body.password,
@@ -60,37 +53,10 @@ module.exports = function (app) {
         }).then(function () {
             res.redirect(307, "/api/login");
         }).catch(function (err) {
-            console.log(err);
             res.json(err);
         });
     });
 
-    app.get("/logout", function (req, res) {
-        req.logout();
-        res.redirect("/");
-    });
-
-
-    app.get("/api/user_data", function (req, res) {
-        if (!req.user) {
-            res.json({});
-        } else {
-            res.json({
-                email: req.user.email,
-                id: req.user.id
-            });
-        }
-    });
-
-    //trip routes
-    app.get("/api/trip_data", function(req,res){
-        db.Trip.findAll({}).then(function(trip){
-            // grab most recent trip
-            res.json(trip.reverse()[0]);
-        })
-    });
-    
-    // use this route when save new restaurant. Tested:working
     app.post("/api/restaurant", function (req, res) {
         db.Restaurant.create(req.body).then(function (result) {
             res.json({
@@ -98,7 +64,7 @@ module.exports = function (app) {
             })
         })
     })
-    // use this route when save new event.  Tested:working
+
     app.post("/api/activity", function (req, res) {
         db.Activity.create(req.body).then(function (result) {
             res.json({
@@ -106,7 +72,7 @@ module.exports = function (app) {
             })
         })
     })
-    // use this route when create new trip Tested:working
+
     app.post("/api/trip", function (req, res) {
         db.Trip.create(req.body).then(function(result){
             res.json({
