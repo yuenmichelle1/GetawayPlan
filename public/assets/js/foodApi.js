94583cd55532e241302949514facd3ef95acd25d
Sean Arca <sean.arca@northwestern.edu>
2018-03-22 22:08:11 -0500

    foodapi css done

--- public/assets/js/foodApi.js
+++ public/assets/js/foodApi.js
@@ -12,7 +12,7 @@ $(function () {
     var addNewRestuarantRows = function (response) {
         var result = response.results;
         //change how many result to show. 20 max for one call. 60 max total (3 calls)
-        for (var i = 0; i < 2; i++) {
+        for (var i = 0; i < 10; i++) {
             var photoRef = result[i].photos[0].photo_reference;
             var photoURL = getPhotoURLByReference(photoRef);
             var restInfo = {
@@ -22,9 +22,9 @@ $(function () {
                 "data-photourl": photoURL
             }
             var newRestDiv = $("<div class='row restaurant'>");
-            var photo_col = $("<div class='col-md-3'>")
-            var info_col = $("<div class='col-md-3'>")
-            var select_col = $("<div class='col-md-6'>")
+            var photo_col = $("<div class='col-md-3 rest-pic'>")
+            var info_col = $("<div class='col-md-6 rest-info'>")
+            var select_col = $("<div class='col-md-3 rest-btn'>")
 
             addInfoToCols(restInfo, photo_col, info_col);
             addSaveButton(restInfo, select_col)
@@ -34,9 +34,9 @@ $(function () {
         }
     }
     var addInfoToCols = function (restInfo, photo_col, info_col) {
-        var restNameDiv = $("<h4>").text(restInfo["data-restname"]);
-        var restAddressDiv = $("<p>").text(restInfo["data-restaddress"]);
-        var restRatingDiv = $("<p>").text(restInfo["data-restrating"]);
+        var restNameDiv = $("<h4 class='rest-name'>").text(restInfo["data-restname"]);
+        var restAddressDiv = $("<h2 class='rest-address'>").text(restInfo["data-restaddress"]);
+        var restRatingDiv = $("<h3 class='rest-rating'>").text(restInfo["data-restrating"]);
         var imgDiv = $(`<img src=${restInfo["data-photourl"]} alt="restaurant-photo">`);
         photo_col.append(imgDiv);
         info_col.append(restNameDiv, restAddressDiv, restRatingDiv);
@@ -44,14 +44,15 @@ $(function () {
     }
 
     var addSaveButton = function (restInfo, select_col) {
-        var button = $("<button class='btn-primary saveRestaurant'>").text("Save");
+        var button = $("<button class='btn btn-warning my-2 my-sm-0 nav-btn login-btn saveRestaurant'>").text("+ Add To My Trip");
         button.attr(restInfo);
         select_col.append(button);
     }
 
     var addGetMoreBtn = function (btnClass) {
-        var moreSuggestionButton = $(`<button class='btn-primary btn-block ${btnClass}'>`).text("Show me more");
-        $(".diningOptions").append(moreSuggestionButton);
+        var moreSuggestionButton = $(`<button class='btn btn-block btn-primary my-2 my-sm-0 nav-btn dashboard-btn more-btn ${btnClass}'>`).text("Show Me More");
+        var moreSuggestionDiv = $("<div class='more-div'></div>").append(moreSuggestionButton);
+        $(".diningOptions").append(moreSuggestionDiv);
     }
 
     var saveRestaurant = function () {
